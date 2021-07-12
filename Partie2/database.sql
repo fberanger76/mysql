0ca71b1b985e5702abbdcb2ac6fbceb76c1c404a
Franck <franckberanger@imac-de-franck.home>
2021-07-12 14:51:01 +0200

    modification valeurs INT

--- Partie2/database.sql
+++ Partie2/database.sql
@@ -2,37 +2,37 @@ exercice 1:
 USE `webDevelopment`;
 CREATE TABLE `languages` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `language` VARCHAR(40)
+    `language` VARCHAR(255)
 );
 
 exercice 2 :
 CREATE TABLE `tools` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `tool` VARCHAR(40)
+    `tool` VARCHAR(255)
 );
 
 exercice 3 :
 CREATE TABLE `frameworks` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(40)
+    `name` VARCHAR(255)
 );
 
 exercice 4 :
 CREATE TABLE `librairies` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `librairy` VARCHAR(40)
+    `librairy` VARCHAR(255)
 );
 
 exercice 5 :
 CREATE TABLE `ide` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `ideName` VARCHAR(40)
+    `ideName` VARCHAR(255)
 );
 
 exercice 6:
 CREATE TABLE IF NOT EXISTS `frameworks` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(40)
+    `name` VARCHAR(255)
 );
 
 exercice 7:
@@ -49,11 +49,11 @@ CREATE DATABASE `codex`;
 USE `codex`;
 CREATE TABLE `clients` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `lastname` VARCHAR(40),
-    `firstname` VARCHAR(40),
+    `lastname` VARCHAR(255),
+    `firstname` VARCHAR(255),
     `birthDate` DATETIME,
-    `address` VARCHAR(40),
-    `firstPhoneNumber` INT,
-    `secondPhoneNumber` INT,
-    `mail` VARCHAR(40)
+    `address` VARCHAR(255),
+    `firstPhoneNumber` INT(10),
+    `secondPhoneNumber` INT(10),
+    `mail` VARCHAR(255)
 );
