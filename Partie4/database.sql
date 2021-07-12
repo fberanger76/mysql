1a67324bcda92c18887be4dacb8939da7cf7776b
Franck <franckberanger@imac-de-franck.home>
2021-07-12 16:43:40 +0200

    partie 4 ok

---
+++ Partie4/database.sql
@@ -0,0 +1,18 @@
+exercice 1:
+USE `webDevelopment`;
+INSERT INTO `languages` 
+VALUES (1, 'Javascript', 'version 5'),
+       (2, 'PHP', 'version 5.2'),
+       (3, 'PHP', 'version 5.4'),
+       (4, 'HTML', 'version 5.1'),
+       (5, 'Javascript', 'version 6'),
+       (6, 'Javascript', 'version 7'),
+       (7, 'Javascript', 'version 8'),
+       (8, 'PHP', 'version 7');
+
+exercice 2:
+INSERT INTO `frameworks` 
+VALUES (1, 'Symfony', '2.8'),
+       (2, 'Symfony', '3'),
+       (3, 'Jquery', '1.6'),
+       (4, 'Jquery', '2.1');
